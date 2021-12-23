b81f3e76af5e98b0671d61780754dea4d08dadef
matmang <matmang@hanyang.ac.kr>
2021-12-23 17:05:11 +0900

    function practice

--- main.go
+++ main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func lenAndUpper(name string) (int, string) {
+	return len(name), strings.ToUpper(name)
+}
+
+func multiply(a, b int) int {
+	return a * b
+}
+
+func repeatMe(words ...string) {
+	fmt.Println(words)
+}
+
+func main() {
+	totalLength, upperName := lenAndUpper("jiwoo")
+	fmt.Println(multiply(2, 2))
+	fmt.Println(totalLength, upperName)
+	repeatMe("jiwoo", "nico", "abc", "bdc")
+}
